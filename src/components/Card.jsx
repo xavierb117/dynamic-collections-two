a613cb1752e0ac1a6b1d2a06e84bc5360ccdc59b
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-14 13:59:07 -0700

    high attack tag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,32 @@
 export default function Card({id, name, type, hp, attack, defense, image, abilities}) {
-    function typeToList(type) {
-        type.map((element) => {
-            return (<li key = {id}>{element}</li>)
-        })
+    const typeToList = type.map((t)=>{
+        return(
+            <>
+                <li>{t}</li>
+            </>
+        )
+    })
+    function highAttack(attack){
+        if(attack>60){
+            return(<p>High Attack Damage!</p>)
+        }
     }
 
     return (
         <>
             <h3>{name}</h3>
-            <ul>
-                {typeToList(type)}
-            </ul>
-            <p>{hp}</p>
-            <p>{attack}</p>
-            <p>{defense}</p>
             <img src = {image} alt = {`Picture of ${name}`}></img>
-            <p>{abilities[0].name} {abilities[0].description}</p>
-            <p>{abilities[1].name} {abilities[1].description}</p>
+            <h4>Type:</h4>
+            <ul>
+                {typeToList}
+            </ul>
+            <h4>Stats:</h4>
+            <p>HP: {hp}</p>
+            <p>Attack: {attack} {highAttack(attack)}</p>
+            <p>Defense: {defense}</p>
+            <h4>Abilities:</h4>
+            <p>{abilities[0].name}: {abilities[0].description}</p>
+            <p>{abilities[1].name}: {abilities[1].description}</p>
         </>
     )
 }
